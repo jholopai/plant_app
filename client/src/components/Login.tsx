1965bfb798bb8e96af6e06a2f8583e6054efbc8c
jholopai <jholopai@users.noreply.github.com>
2023-12-31 17:12:23 +0200

    add registration and login forms, add initial client-side validation

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {Form, Button} from 'react-bootstrap'
+import {useForm, SubmitHandler} from 'react-hook-form'
 
 const Login = () => {
+
+	interface LoginData {
+		username: string,
+		password: string,
+	}
+
+	const {register, handleSubmit, formState:{errors}} = useForm<LoginData>()
+
+	const login: SubmitHandler<LoginData>  = (data) => {
+		console.log("Logged in!")
+	}
+
 	return (
-		<h1>Login</h1>
+		<div className="container">
+			<h1>Login</h1>
+			<div className="form">
+				<form onSubmit={handleSubmit(login)}>
+					<Form.Group>
+						<Form.Label>Username</Form.Label>
+						<Form.Control type="text"
+									  {...register("username", {required:true})}/>
+						{errors.username?.type === "required" && <span>Please provide a username.</span>}
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Password</Form.Label>
+						<Form.Control type="password"
+									  {...register("password", {required:true})}/>
+						{errors.password && <span>Please provide a password.</span>}
+					</Form.Group>
+					<Form.Group>
+						<Button type="submit" variant="primary">Log in</Button>
+					</Form.Group>
+					<Form.Group>
+						<small>Don't have an account? <Link to='/register'>You can register one here!</Link></small>
+					</Form.Group>
+				</form>
+			</div>
+		</div>
 	)
 }
 
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,8 +1,82 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {Form, Button} from 'react-bootstrap'
+import {SubmitHandler, useForm} from 'react-hook-form'
 
 const Register = () => {
+
+	interface FormData {
+		email: string,
+		username: string,
+		password: string,
+		confirmPassword: string
+	}
+
+	const {register, watch, handleSubmit, reset, formState:{errors}} = useForm<FormData>()
+
+	const registration: SubmitHandler<FormData> = (data) => {
+		console.log("Registration!")
+		reset()
+	}
+
 	return (
-		<h1>Register</h1>
+		<div className="container">
+			<h1>Register</h1>
+			<div className="form">
+				<form onSubmit={handleSubmit(registration)}>
+					<Form.Group>
+						<Form.Label>Username</Form.Label>
+						<Form.Control type="text"
+									  placeholder="Your username"
+									  {...register("username", {required:true,
+									  							minLength:5,
+																maxLength:25,
+																pattern: /[a-zA-Z0-9]*/})}/>
+						{errors.username?.type=="required" && <span>Username is required.</span>}
+						{(errors.username?.type=="minLength" || errors.username?.type === "maxLength") && <span>Username must be between 5 and 25 characters long.</span>}
+						{errors.username?.type=="pattern" && <span>Username can only contain alphabeticals and numerals.</span>}
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Email</Form.Label>
+						<Form.Control type="email"
+									  placeholder="Your email"
+									  {...register("email", {required:true,
+															 minLength:6,
+									  						 maxLength:60,
+									 						 pattern: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/})}/>
+						{errors.email?.type=="required" && <span>Email is required.</span>}
+						{(errors.email?.type=="minLength" || errors.email?.type === "maxLength") && <span>Email must be between 6 and 60 characters long.</span>}
+						{errors.email?.type=="pattern" && <span>Email is not valid.</span>}
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Password</Form.Label>
+						<Form.Control type="password"
+									  placeholder="Your password"
+									  {...register("password", {required:true,
+																minLength:10,
+										  						maxLength:25})}/>
+						{errors.password?.type=="required" && <span>Password is required.</span>}
+						{(errors.password?.type=="minLength" || errors.password?.type === "maxLength") && <span>Password must be between 10 and 25 characters long.</span>}
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Confirm password</Form.Label>
+						<Form.Control type="password"
+									  placeholder="Confirm password"
+									  {...register("confirmPassword", {required:true,
+																	   validate: (value) => value === watch("password") || "Passwords do not match."
+																	   })}/>
+						{errors.confirmPassword?.type=="required" && <span>Please confirm your password.</span>}
+						{errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
+					</Form.Group>
+					<Form.Group>
+						<Button type="submit" variant="primary">Register</Button>
+					</Form.Group>
+					<Form.Group>
+						<small>Already have an account? <Link to='/login'>You can log in here!</Link></small>
+					</Form.Group>
+				</form>
+			</div>
+		</div>
 	)
 }
 
